2527507d81d11da7fd710155be58fd2845370712
Paulo Lourenço <paulohll14@gmail.com>
2019-02-23 11:40:56 -0300

    Initial Commit

---
+++ PHLCheckbox/PHLCheckbox.h
@@ -0,0 +1,19 @@
+//
+//  PHLCheckbox.h
+//  PHLCheckbox
+//
+//  Created by Paulo Lourenço on 23/02/19.
+//  Copyright © 2019 Paulo Lourenço. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PHLCheckbox.
+FOUNDATION_EXPORT double PHLCheckboxVersionNumber;
+
+//! Project version string for PHLCheckbox.
+FOUNDATION_EXPORT const unsigned char PHLCheckboxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PHLCheckbox/PublicHeader.h>
+
+
